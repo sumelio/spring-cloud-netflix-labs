eae6285f0a6abfd1989b4e04924e7ab259a13074
Freddy Lemus <sumel124@gmail.com>
2019-04-24 07:13:41 -0500

    Add project sentences and config clients

---
+++ lab-4-sentence/src/main/java/com/eureka/client/lab4sentence/Lab4SentenceApplication.java
@@ -0,0 +1,13 @@
+package com.eureka.client.lab4sentence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4SentenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab4SentenceApplication.class, args);
+	}
+
+}
