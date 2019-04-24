562aa7435d320175af0859ff095e994a0a623cf2
Freddy Lemus <sumel124@gmail.com>
2019-04-24 06:30:44 -0500

    Add project eureka client and config eureka server

---
+++ lab-4-subject/src/main/java/com/eureka/client/lab4subject/Lab4SubjectApplication.java
@@ -0,0 +1,13 @@
+package com.eureka.client.lab4subject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4SubjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab4SubjectApplication.class, args);
+	}
+
+}
