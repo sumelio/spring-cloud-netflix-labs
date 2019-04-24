d2b5d24b925e90e4f9a856fdab35fb57e113cb7a
Freddy Lemus <sumel124@gmail.com>
2019-04-24 06:18:14 -0500

    Add project eureka server

---
+++ lab-4-eureka-server/src/main/java/com/eureka/server/lab4eurekaserver/Lab4EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.eureka.server.lab4eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab4EurekaServerApplication.class, args);
+	}
+
+}
