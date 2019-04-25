73ec9eae04c8da7639a1028073c9ecebc5011a1a
Freddy Lemus <sumel124@gmail.com>
2019-04-24 23:24:52 -0500

    New version

--- lab-4-adjective/src/main/java/com/eureka/client/lab4adjective/Lab4AdjectiveApplication.java
+++ lab-4-adjective/src/main/java/com/eureka/client/lab4adjective/Lab4AdjectiveApplication.java
@@ -1,8 +1,9 @@
 package com.eureka.client.lab4adjective;
 
+
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+		import org.springframework.boot.autoconfigure.SpringBootApplication;
+		import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,5 +12,4 @@ public class Lab4AdjectiveApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(Lab4AdjectiveApplication.class, args);
 	}
-
 }
