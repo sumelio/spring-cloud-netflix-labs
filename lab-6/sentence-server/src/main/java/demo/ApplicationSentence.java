14fc20e89bc58210a99d6e1acf92ac9361e2619d
Freddy Lemus <sumel124@gmail.com>
2019-05-02 20:35:47 -0500

    Add new client Feign

---
+++ lab-6/sentence-server/src/main/java/demo/ApplicationSentence.java
@@ -0,0 +1,17 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class ApplicationSentence {
+
+    public static void main(String[] args) {
+
+       SpringApplication.run(ApplicationSentence.class, args);
+    }
+}
